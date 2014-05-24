e8a04a3f907b50c398900d4045b66a86c9256842
Henry Chan <hchan@apache.org>
2014-05-24 15:05:14 -0700

    a

--- js/view/app-view.js
+++ js/view/app-view.js
@@ -8,24 +8,29 @@ define
      ({
 	                
          initialize: function() {             
-	     
+			
              this.template = appTmpl; 
 
-	     this.render();
+			this.render();
+			if (App.isLoggedIn()) {
+				$("#login").html("<a href='#profile'>View Profile</a>");
+			} else {
+				$("#login").html("<a href='#login'>Login</a>");
+			}
 
-	     $("#searchButton").click(function() {
-		 var books = new booksClass();
-		 var options = {};
-		 options.q = $("#search").val();
-		 var callback = function(data) {
-		     var x2js = new X2JS();
-		     var jsonObj = x2js.xml2json(data)
-		     var booksHtml = booksTmpl(jsonObj);
-		     $("#main").html(booksHtml);
-		 }
-		 books.fetch(options, callback);
-	     });      
-         },
+			 $("#searchButton").click(function() {
+			 var books = new booksClass();
+			 var options = {};
+			 options.q = $("#search").val();
+			 var callback = function(data) {
+				 var x2js = new X2JS();
+				 var jsonObj = x2js.xml2json(data)
+				 var booksHtml = booksTmpl(jsonObj);
+				 $("#main").html(booksHtml);
+			 }
+			 books.fetch(options, callback);
+			 });      
+		},
 
      });               
      return AppView;
--- js/view/book-view.js
+++ js/view/book-view.js
@@ -8,27 +8,31 @@ define
 	                
          initialize: function() {             
              $("#main").html(bookTmpl());
-	     var bookModel = new BookClass();
-	     var options = {};
+			 
+			 if (App.isLoggedIn()) {
+				$("#createReviewDiv").show();
+			}
+			 var bookModel = new BookClass();
+			 var options = {};
 
-	     options.id = this.options.id;
-	     var callback = function(data) {
-		 var x2js = new X2JS();
-		 var jsonObj = x2js.xml2json(data)
-		 //	     var booksHtml = booksTmpl(jsonObj);
-		 // this is a composite view, but instead of rendering "#book" with a template,
-		 // let's do it the simple way
-		 console.log(jsonObj.GoodreadsResponse.book);
-		 $("#book").html(jsonObj.GoodreadsResponse.book.title.toString());
+			 options.id = this.options.id;
+			 var callback = function(data) {
+			 var x2js = new X2JS();
+			 var jsonObj = x2js.xml2json(data)
+			 //	     var booksHtml = booksTmpl(jsonObj);
+			 // this is a composite view, but instead of rendering "#book" with a template,
+			 // let's do it the simple way
+			 console.log(jsonObj.GoodreadsResponse.book);
+			 $("#book").html(jsonObj.GoodreadsResponse.book.title.toString());
 
-		 $("#reviews").html(jsonObj.GoodreadsResponse.book.reviews_widget.toString());
-		 
+			 $("#reviews").html(jsonObj.GoodreadsResponse.book.reviews_widget.toString());
+			 
 
 
-	     }
-	     bookModel.fetch(options, callback);
-             
-         },
+			 }
+			 bookModel.fetch(options, callback);
+				 
+			 },
 
      });               
      return retval;
