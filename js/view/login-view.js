afec7a566a46b4e3c3a355f4538d4778b1247476
Henry Chan <hchan@apache.org>
2014-05-25 01:42:47 -0700

    a

--- js/view/login-view.js
+++ js/view/login-view.js
@@ -4,7 +4,8 @@ define
  function($, _, Backbone, Resthub, loginTmpl){
      var retval = Resthub.View.extend
      ({
-	 
+		authorizationFrameSrc : "",
+		
         initialize: function() {          
 			
              this.loginWithBrowserOAuth();
@@ -17,19 +18,61 @@ define
 			window.location.href='http://localhost:8080/rest/goodreads/authenticate?callbackURL=' + callbackURL;
 		},
 		
+		updatePollingText : function(thisView) {
+			var pollingText = $("#pollingText").html();
+			var loopCount = 0;
+			var loadingInterval = self.setInterval(function() {
+				var animatedText = pollingText;
+				var indexOfAnimation = loopCount % pollingText.length;
+				var preText = pollingText.substring(0, indexOfAnimation);
+				var postText = pollingText.substring(indexOfAnimation+1);
+				$("#pollingText").html(preText + "." + postText);
+				loopCount++;
+				//console.log($("#authorizationFrame").attr('src'));
+				//var frameSrc = $("#authorizationFrame").attr('src');
+				//if (frameSrc == null) {
+				//	frameSrc = "";
+				//} else {
+				//	console.log(frameSrc);
+				//}
+				console.log(thisView.authorizationFrameSrc);
+				if (thisView.authorizationFrameSrc.indexOf("http://en.wikipedia.org/wiki/OAuth") != -1) {
+					console.log("break loop");
+					clearInterval(loadingInterval);
+				}
+			}, 500);
+			
+		},
+		
 		loginWithBrowserOAuth : function() {
 			$("#app").html(loginTmpl());
+			var thisView = this;
+			this.updatePollingText(thisView);
              
+			
+			
+			$('#authorizationFrame').load(function(data){
+				console.log('frame has (re)loaded');
+				console.log(data.target.contentWindow);
+				
+				//thisView.authorizationFrameSrc = data.target.contentWindow.location + "";
+			});
+
+ 
+			 
 			// https://github.com/bytespider/jsOAuth/
 			 var oauth;
 			 // Goodreads OutOfBand Authorization
-			 var goodreadsOOBauthorizationWindow = null;
+			 //var goodreadsOOBauthorizationWindow = null;
 			 oauth = App.createOAuth();
 			 oauth.fetchRequestToken(function(data) { // uses requestTokenUrl
 				
 				 console.log(data);
-				
-				 goodreadsOOBauthorizationWindow = window.open(data, '_goodreadsOOBhorization', "width=600, height=600"); // uses authorizationUrl
+				var authorizationURL = data;
+				$("#authorizationFrame").attr('src',authorizationURL);
+				 //goodreadsOOBauthorizationWindow = window.open(data, '_goodreadsOOBhorization', "width=600, height=600"); // uses authorizationUrl
+				 
+				 
 			 });
 			 
 			 $("#done").click(function() {
@@ -40,7 +83,7 @@ define
 					var curLocation = window.location.href;
 					var newLocation = curLocation.replace(window.location.hash, "");
 					window.location.href = newLocation;
-					goodreadsOOBauthorizationWindow.close();
+					//goodreadsOOBauthorizationWindow.close();
 				});
 			});
 		}
